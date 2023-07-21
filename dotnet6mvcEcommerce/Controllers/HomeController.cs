7033fd908803585553798498eb1b11aa0a29053b
Quannn <quannn0180716v@gmail.com>
2023-07-21 15:39:16 +0800

    logger、ViewBag,ViewData了解

--- dotnet6mvcEcommerce/Controllers/HomeController.cs
+++ dotnet6mvcEcommerce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using dotnet6mvcEcommerce.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,13 +14,22 @@ namespace dotnet6mvcEcommerce.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        //[Authorize(Roles ="Admin")]
+        public IActionResult Index(int? id)
         {
-            return View();
+            Student student = new Student() { Id = 1, Name = "Quan程式設計" };
+            ViewData["Name"] = "ViewData---Quan程式設計";
+            ViewBag.name = "ViewBag--Quan程式設計";
+            return View(student);
+            //return Json(student);
         }
 
         public IActionResult Privacy()
         {
+            //可以寫入文本文件、資料庫、控制台
+            _logger.LogError("嚴重錯誤");
+            _logger.LogWarning("警告錯誤");
+            _logger.LogInformation("在HomeController的Privacy方法內");
             return View();
         }
 
@@ -28,5 +38,11 @@ namespace dotnet6mvcEcommerce.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
