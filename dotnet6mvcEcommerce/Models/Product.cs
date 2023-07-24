775145156256a6e29841bdbd8ae3c89b7dbf4b0c
Quannn <quannn0180716v@gmail.com>
2023-07-24 15:10:57 +0800

    圖片顯示及中文標題

--- dotnet6mvcEcommerce/Models/Product.cs
+++ dotnet6mvcEcommerce/Models/Product.cs
@@ -1,15 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotnet6mvcEcommerce.Models
 {
     public class Product
     {
+        [Display(Name="商品Id")]
         public int Id { get; set; }
+
+        [Display(Name = "商品名稱")]
         public string Name { get; set; }
+
+        [Display(Name = "價格")]
         public decimal Price { get; set; }
+
+        [Display(Name = "庫存")]
         public short Stock { get; set; }
+
+        [Display(Name = "圖片URL")]
         public string? ImageUrl { get; set; }
+
         [NotMapped]
+        [Display(Name = "圖片")]
         public IFormFile? FormFile { get; set; }
     }
 }
